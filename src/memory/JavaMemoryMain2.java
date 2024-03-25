355b2f35914dc9337715d90b861beab8f04dbff1
fbehddn <tom0611@naver.com>
2024-03-25 16:01:21 +0900

    section7: 스택영역 힙영역

---
+++ src/memory/JavaMemoryMain2.java
@@ -0,0 +1,32 @@
+package memory;
+
+public class JavaMemoryMain2 {
+    public static void main(String[] args) {
+        System.out.println("main start");
+        method1();
+        System.out.println("main end");
+    }
+
+    static void method1() {
+        System.out.println("method1 start");
+        Data data1 = new Data(10);
+        method2(data1);
+        System.out.println("method1 end");
+    }
+
+    static void method2(Data data2) {
+        System.out.println("method2 start");
+        System.out.println("data2.value() = " + data2.getValue());
+        System.out.println("method2 end");
+    }
+
+    /**
+     * main start
+     * method1 start
+     * method2 start
+     * data2.value() = 10
+     * method2 end
+     * method1 end
+     * main end
+     */
+}
